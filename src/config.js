dce01c2094840f5fbc396052a29976eaac140a78
Eduard Kotkas <edkotkas@gmail.com>
2018-10-26 00:03:00 +0100

    adding project alias functionality

--- src/config.js
+++ src/config.js
@@ -7,9 +7,10 @@ const project = require('../package')
 const defaultConfig = require('./defaultConfig')
 defaultConfig.aliaVersion = project.version
 
-let config = defaultConfig
+const aliaFileName = '.alia.json'
 
-const configPath = path.join(homedir, '.alia.json')
+let config = defaultConfig
+const configPath = path.join(homedir, aliaFileName)
 
 try {
   config = require(configPath)
@@ -19,6 +20,21 @@ try {
     writeConfig()
   } else {
     console.error('Error reading config', err)
+  }
+}
+
+const currentDir = process.cwd()
+const projectConfigPath = path.join(currentDir, aliaFileName)
+
+let projectConfig
+
+if (projectConfigPath !== configPath) {
+  try {
+    projectConfig = require(projectConfigPath)
+  } catch(err) {
+    if(err.code !== 'MODULE_NOT_FOUND') {
+      console.error('Error reading project config', err)
+    }
   }
 }
 
@@ -38,8 +54,8 @@ function help() {
         --version, -v     show version
         --help, -h        show this
         
-        --add, -a         add alias
-        --remove, -r      remove alias
+        --add, -a         add alias (add -p for project alias)
+        --remove, -r      remove alias (add -p for project alias)
         --list, -l        list available alias
         --separator, -s   change the separator (default: @)
         --pull, -p        restore your config
@@ -58,59 +74,88 @@ function help() {
   `)
 }
 
-function writeConfig() {
+function writeConfig(proj = false) {
   config.aliaVersion = project.version
-  fs.writeFileSync(configPath, JSON.stringify(config, null, 2))
+  fs.writeFileSync(proj ? projectConfigPath : configPath, JSON.stringify(project ? projectConfig : config, null, 2))
 }
 
 function addAlias(args) {
+  const project = args[0] === '-p'
   let separatorIndex = args.indexOf(config.options.separator)
   if (separatorIndex === -1) {
     console.error(`Invalid Input, missing separator: '${config.options.separator}'`)
     return 1
   }
 
-  const key = args.slice(0, separatorIndex).join(' ')
+  const key = args.slice(project ? 1 : 0, separatorIndex).join(' ')
   const cmd = args.slice(separatorIndex + config.options.separator.length - 1)
 
-  if (config.alias[key]) {
-    console.error(`Alias '${key}' already exists - remove and try again`)
-    return 1
+  if (project) {
+    if (projectConfig.alias[key]) {
+      console.error(`Alias '${key}' already exists - remove and try again`)
+      return 1
+    } else {
+      projectConfig.alias[key] = cmd
+      writeConfig(projectConfigPath)
+    }
   } else {
-    config.alias[key] = cmd
-    writeConfig()
+    if (config.alias[key]) {
+      console.error(`Alias '${key}' already exists - remove and try again`)
+      return 1
+    } else {
+      config.alias[key] = cmd
+      writeConfig(configPath)
+    }
   }
+
 
   console.log(`Added alias: ${key} ${config.options.separator} ${cmd.join(' ')}`)
   return 0
 }
 
 function removeAlias(args) {
-  const key = args.join(' ').trim()
+  const project = args[0] === '-p'
+  const key = project ? args.slice(1) : args.join(' ').trim()
 
   if (!key) {
     console.error('No alias specified')
     return 1
   }
-
-  if (!config.alias[key]) {
-    console.error(`Alias '${key}' does not exist`)
-    return 1
+  if (project) {
+    if (!projectConfig.alias[key]) {
+      console.error(`Alias '${key}' does not exist`)
+      return 1
+    } else {
+      delete projectConfig.alias[key]
+      writeConfig(projectConfigPath)
+    }
   } else {
-    delete config.alias[key]
-    writeConfig()
+    if (!config.alias[key]) {
+      console.error(`Alias '${key}' does not exist`)
+      return 1
+    } else {
+      delete config.alias[key]
+      writeConfig(configPath)
+    }
   }
 
   console.log(`Removed alias: ${key}`)
   return 0
 }
 
-function list() {
-  const alias = Object.keys(config.alias)
-    .map(key => `${key} \t${config.options.separator} \t${config.alias[key].join(' ')}`)
-    .join('\n')
+function mapList(alias) {
+  return Object.keys(alias).map(key => `${key} \t${config.options.separator} \t${alias[key].join(' ')}`)
+}
 
-  console.log(alias)
+function list() {
+  let alias = ['Global', ...mapList(config.alias)]
+
+  if (projectConfig && projectConfig.alias) {
+    const projectAlias = mapList(projectConfig.alias)
+    alias.push('\nProject', ...projectAlias)
+  }
+
+  console.log(alias.join('\n'))
 }
 
 function setSeparator(args) {
@@ -154,3 +199,4 @@ function gistPush() {
 module.exports.alias = { addAlias, removeAlias, list, help, version }
 module.exports.options = { setSeparator, gistPull, gistPush }
 module.exports.config = config
+module.exports.projectConfig = projectConfig
